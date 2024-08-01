0cfc272bf9def3800829fcfa09fd422019f42bfe
Melee Fox <fox.melee@yandex.ru>
2024-08-01 20:26:25 +0300

    Добавление метода запуска программы в класс Application

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Application {
-    
+    //Метод запуска системы
+    public void exec_app(){
+        String input ="";
+        String action_input = "";
+        Scanner scan = new Scanner(System.in);
+        while(input != "!"){
+            input = scan.nextLine();
+            Calculator.SetNum1(Integer.parseInt(input));
+            input = scan.nextLine();
+            action_input = input;
+            input = scan.nextLine();
+            Calculator.SetNum2(Integer.parseInt(input));
+            
+        }
+    }
+
 }
